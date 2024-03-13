cad5c4e702e6614235162df1ac026cd0b461bc53
Florine Kieraga <florine.kieraga@epita.fr>
2024-03-13 14:50:25 +0100

    [Feat] class DancingLinksDotNetSolver + function solve

--- Sudoku.DancingLinks/DancingLinksDotNetSolver.cs
+++ Sudoku.DancingLinks/DancingLinksDotNetSolver.cs
@@ -1,5 +1,26 @@
-﻿namespace Sudoku.DancingLinks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sudoku.Shared;
 
-public class DancingLinksDotNetSolver
+namespace Sudoku.DancingLinks
 {
+    public class DancingLinksDotNetSolver : ISudokuSolver
+    {
+        /// <summary>
+        /// Solves the given Sudoku grid using a backtracking algorithm.
+        /// </summary>
+        /// <param name="s">The Sudoku grid to be solved.</param>
+        /// <returns>
+        /// The solved Sudoku grid.
+        /// </returns>
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+        
+    }
 }
+
