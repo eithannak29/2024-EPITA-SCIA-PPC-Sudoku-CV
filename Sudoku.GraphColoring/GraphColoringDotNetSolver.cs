ec7cc690887996677d54be2406badfae07e32259
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-03-13 15:14:14 +0100

    build(graph-coloring): create solver project

---
+++ Sudoku.GraphColoring/GraphColoringDotNetSolver.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.GraphColoring;
+
+public class GraphColoringDotNetSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid grid)
+    {
+        throw new NotImplementedException();
+    }
+}
