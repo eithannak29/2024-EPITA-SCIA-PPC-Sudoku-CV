b23a295f63453b19ca9c903e885f2209b46dc0b2
Clovis Lechien <clovis.lechien@epita.fr>
2024-03-15 12:58:52 +0100

    feat(dlx-solver): added multithreading where possible lol, hopefully this does the trick

--- Sudoku.DancingLinks/DancingLinksDotNetSolver.cs
+++ Sudoku.DancingLinks/DancingLinksDotNetSolver.cs
@@ -30,9 +30,9 @@ namespace Sudoku.DancingLinks
             // Pourquoi 729 : 9 * 9 (=81) cases, et chacune des cases peut avoir 9 chiffres => 9 * 9 * 9 = 81 * 9 = 729 
             // Pourquoi 324 : chaque case (9 * 9 = 81) est soumise à 4 contraintes : case, box, row, column => 4 * 81 = 324
 
-            for (int i = 0; i < 9; i++)
+            Parallel.For(0, 9, i =>
             {
-                for (int j = 0; j < 9; j++)
+                Parallel.For(0, 9, j =>
                 {
                     int cellValue = inputSudoku.Cells[i, j];
                     if (cellValue != 0)
@@ -41,6 +41,7 @@ namespace Sudoku.DancingLinks
                         int colIndex1 = 81 + j * 9 + cellValue - 1;
                         int colIndex2 = 81 * 2 + i * 9 + cellValue - 1;
                         int colIndex3 = 81 * 3 + ((i / 3) + (j / 3) * 3) * 9 + cellValue - 1;
+
                         dlxMatrix[rowIndex * 9, rowIndex] = 1;
                         dlxMatrix[rowIndex * 9, colIndex1] = 1;
                         dlxMatrix[rowIndex * 9, colIndex2] = 1;
@@ -54,14 +55,16 @@ namespace Sudoku.DancingLinks
                             int colIndex1 = 81 + j * 9 + val - 1;
                             int colIndex2 = 81 * 2 + i * 9 + val - 1;
                             int colIndex3 = 81 * 3 + ((i / 3) + (j / 3) * 3) * 9 + val - 1;
+
                             dlxMatrix[rowIndex * 9 + val - 1, rowIndex] = 1;
                             dlxMatrix[rowIndex * 9 + val - 1, colIndex1] = 1;
                             dlxMatrix[rowIndex * 9 + val - 1, colIndex2] = 1;
                             dlxMatrix[rowIndex * 9 + val - 1, colIndex3] = 1;
                         }
                     }
-                }
-            }
+                });
+            });
+            
             return dlxMatrix;
         }
         
@@ -69,7 +72,7 @@ namespace Sudoku.DancingLinks
         {
             var sudokuGrid = new SudokuGrid();
 
-            foreach (int row in solution.RowIndexes)
+            Parallel.ForEach(solution.RowIndexes, row =>
             {
                 int? cellId = Enumerable.Range(0, inputMatrix.GetLength(1))
                     .FirstOrDefault(index => inputMatrix[row, index] == 1, -1);
@@ -79,7 +82,7 @@ namespace Sudoku.DancingLinks
                 cellValue %= 9;
                 
                 sudokuGrid.Cells[(int)(cellId % 9), (int)(cellId / 9)] = (int)(cellValue + 1);
-            }
+            });
 
             return sudokuGrid;
         }
